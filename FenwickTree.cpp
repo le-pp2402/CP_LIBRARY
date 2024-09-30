4c7433a4ff3c38684662be41512ad79bd43ecb50
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-10-01 03:22:19 +0700

    Create FenwickTree.cpp

---
+++ FenwickTree.cpp
@@ -0,0 +1,30 @@
+template <class T> class BIT {
+private:
+  int size;
+  vector<T> bit;
+  vector<T> arr;
+
+public:
+  BIT(int size) : size(size), bit(size + 1), arr(size) {}
+
+  void set(int ind, T val) { add(ind, val - arr[ind]); }
+
+  void add(int ind, T val) {
+    arr[ind] += val;
+    ind++;
+    for (; ind <= size; ind += ind & -ind) {
+      bit[ind] += val;
+    }
+  }
+
+  T sum(int ind) {
+    ind++;
+    T total = 0;
+    for (; ind > 0; ind -= ind & -ind) {
+      total += bit[ind];
+    }
+    return total;
+  }
+
+  T sum(int l, int r) { return sum(r) - sum(l - 1); }
+};
