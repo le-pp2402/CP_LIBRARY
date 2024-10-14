f5f2d91c6ec8a59dbb5efe39fbfe9ff44bc3c6be
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-10-15 05:46:43 +0700

    Create AhoCorasick.cpp
    add aho-corasick

---
+++ AhoCorasick.cpp
@@ -0,0 +1,59 @@
+/*
+  Tested: https://open.kattis.com/problems/stringmultimatching
+*/
+constexpr int K = 180;
+struct node {
+  int next[K], go[K], sf, exit[K], jmp;
+  // int output;
+  vector<int> output;
+  node() {
+    fill(next, next + K, -1);
+    fill(go, go + K, -1);
+    fill(exit, exit + K, -1);
+    sf = -1;
+    // output = 0;
+    jmp = -1;
+  }
+};
+
+struct AC {
+  vector<node> g{node()};
+
+  void add(string &s, int out) {
+    int id = 0;
+    for (char &c : s) {
+      if (g[id].next[(int)c] == -1) {
+        g[id].next[(int)c] = g.size();
+        g.push_back(node());
+      }
+      id = g[id].next[(int)c];
+    }
+    g[id].output.push_back(out);
+  }
+
+  void build() {
+    queue<int> qu;
+    qu.push(0);
+    while (!qu.empty()) {
+      int u = qu.front();
+      int sl = g[u].sf;
+      qu.pop();
+      for (int i = 0; i < K; i++) {
+        int nt = g[u].next[i];
+        int go_sf = (u ? g[sl].go[i] : 0);
+
+        if (u) {
+          g[u].jmp = (g[sl].output.size() ? sl : g[sl].jmp);
+        }
+
+        if (nt == -1) {
+          g[u].go[i] = go_sf;
+        } else {
+          g[u].go[i] = nt;
+          g[nt].sf = go_sf;
+          qu.push(nt);
+        }
+      }
+    }
+  }
+};
