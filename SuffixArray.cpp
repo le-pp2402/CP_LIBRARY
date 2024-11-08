d792c087ae89345048893def46a90688e46ae1d5
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-08 21:16:30 +0700

    Create SuffixArray.cpp

---
+++ SuffixArray.cpp
@@ -0,0 +1,71 @@
+// @author: Le Phi Phat
+// @Test:
+// https://codeforces.com/edu/course/2/lesson/2/1/practice/contest/269100/problem/A
+struct SuffixArray {
+  vector<int> dp;
+  vector<int> id;
+  int N, K;
+  SuffixArray(string &s) {
+    s += "$";
+    N = s.size();
+    K = __lg(N) + 1;
+    dp.assign(N, 0);
+    id.assign(N, 0);
+    iota(id.begin(), id.end(), 0);
+    work(s);
+  }
+
+  void counting_sort(vector<array<int, 3>> &a) {
+    for (auto &j : {1, 0}) {
+      vector<int> cnt(N);
+      for (auto &x : a) {
+        cnt[x[j]]++;
+      }
+      vector<int> p(N);
+      for (int i = 1; i < N; i++) {
+        p[i] = p[i - 1] + cnt[i - 1];
+      }
+      vector<array<int, 3>> na(N);
+      for (int i = 0; i < N; i++) {
+        na[p[a[i][j]]++] = a[i];
+      }
+      a.swap(na);
+    }
+  }
+
+  void work(string &s) {
+    sort(id.begin(), id.end(), [&](int i, int j) {
+      return pair<char, int>(s[i], i) < pair<char, int>(s[j], j);
+    });
+
+    for (int i = 1; i < N; i++) {
+      dp[id[i]] = dp[id[i - 1]] + (s[id[i]] != s[id[i - 1]]);
+    }
+
+    for (int i = 1; i <= K; ++i) {
+      int shift = (1 << (i - 1));
+      vector<array<int, 3>> a(N);
+      for (int j = 0; j < N; j++) {
+        a[j][0] = dp[j];
+        a[j][1] = dp[(j + shift) % N];
+        a[j][2] = j;
+      }
+
+      counting_sort(a);
+      for (int j = 0; j < N; j++) {
+        id[j] = a[j][2];
+      }
+
+      dp[id[0]] = 0;
+      for (int j = 1; j < N; j++) {
+        if (a[j][0] == a[j - 1][0] && a[j][1] == a[j - 1][1]) {
+          dp[id[j]] = dp[id[j - 1]];
+        } else {
+          dp[id[j]] = dp[id[j - 1]] + 1;
+        }
+      }
+    }
+  }
+
+  vector<int> get() { return id; }
+};
