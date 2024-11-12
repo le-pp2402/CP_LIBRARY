7be36d1068f5bb9f611ce31497bb1f2288377d52
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-13 00:47:20 +0700

    Create Booth.cpp

---
+++ Booth.cpp
@@ -0,0 +1,27 @@
+/*
+  Booth's lexicographically minimal string rotation algorithm.
+  Time complexity: O($N^3$)
+*/
+int Booth(string s) {
+  int n = s.length();
+  vector<int> f(2 * n, -1);
+  int k = 0;
+  for (int j = 1; j < 2 * n; j++) {
+    int i = f[j - k - 1];
+    while (i != -1 && s[j % n] != s[(k + i + 1) % n]) {
+      if (s[j % n] < s[(k + i + 1) % n]) {
+        k = j - i - 1;
+      }
+      i = f[i];
+    }
+    if (i == -1 && s[j % n] != s[(k + i + 1) % n]) {
+      if (s[j % n] < s[(k + i + 1) % n]) {
+        k = j;
+      }
+      f[j - k] = -1;
+    } else {
+      f[j - k] = i + 1;
+    }
+  }
+  return k;
+}
