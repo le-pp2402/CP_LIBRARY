b58ce4cf9247fe3c1b9d2aa56209d797cfc9a56b
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-13 09:50:47 +0700

    Create KMP.cpp

---
+++ string/KMP.cpp
@@ -0,0 +1,24 @@
+vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+vector<int> pi = prefix_function(s);
+int ans = 0; // So lan s xuat hien trong t
+int j = 0; // Ham tien to o vi tri dang xet cua xau t
+for (int i = 0; i < m; i++) {
+    while (j > 0 && t[i] != s[j])
+        j = pi[j - 1];
+    if (t[i] == s[j])
+        j++;
+    if (j == n)
+        ans++;
+}
