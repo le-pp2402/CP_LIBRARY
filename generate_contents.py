95a8c5d6384eb9bc800a5ec00e63be060053e1b9
doan201203 <doan201203@gmail.com>
2024-11-13 09:55:05 +0700

    Create generate_contents.py

---
+++ generate_contents.py
@@ -0,0 +1,12 @@
+import os
+
+ignores = ['.git'] 
+print(os.listdir('./'))
+
+with open('contents.txt', 'w') as f:
+    for section in os.listdir('./'):
+        if section not in ignores and os.path.isdir(section):
+            f.write('[{}]\n'.format(section))
+            for sub in os.listdir(os.path.join('./', section)):
+                f.write(sub + '\t' + sub.split('.')[0] + '\n')
+    f.close()
