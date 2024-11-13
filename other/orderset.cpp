581d0b35f00e23239e1b1b59f7dfd37806d702a9
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-13 15:58:43 +0700

    Update orderset.cpp

--- other/orderset.cpp
+++ other/orderset.cpp
@@ -1,8 +1,4 @@
-#include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-using namespace std;
 using namespace __gnu_pbds;
- 
-typedef long long ll;
 typedef tree<pair<ll, ll>, null_type,less<pair<ll, ll>>, rb_tree_tag,tree_order_statistics_node_update> ordered_set ;
