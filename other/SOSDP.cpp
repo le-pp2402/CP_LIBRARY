43b2884f1c5f61cfe06143a33404010530ceb62d
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-16 00:16:20 +0700

    Update SOSDP.cpp

--- other/SOSDP.cpp
+++ other/SOSDP.cpp
@@ -4,31 +4,18 @@ the number of elements y such that x & y = x
 the number of elements y such that x & y != 0
 const int B = 20;
 int a[1 << B], f[1 << B], g[1 << B];
-int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  int n; cin >> n;
+int32_t main() { int n; cin >> n;
   for (int i = 1; i <= n; i++) {
-    cin >> a[i];
-    f[a[i]]++; g[a[i]]++;
-  }
+    cin >> a[i]; f[a[i]]++; g[a[i]]++; }
   // sum over subsets
   for (int i = 0; i < B; i++) {
     for (int mask = 0; mask < (1 << B); mask++) {
       if ((mask & (1 << i)) != 0) {
-        f[mask] += f[mask ^ (1 << i)];
-      }
-    }
-  }
+        f[mask] += f[mask ^ (1 << i)]; } } }
   // sum over supersets
   for (int i = 0; i < B; i++) {
     for (int mask = (1 << B) - 1 ; mask >= 0 ; mask--) {
       if ((mask & (1 << i)) == 0) g[mask] += g[mask ^ (1 << i)] ;
-    }
-  }
-  for (int i = 1; i <= n; i++) {
-    cout << f[a[i]] << ' ' << g[a[i]] << ' ' << n - f[(1 << B) - 1 - a[i]] << '\n';
-  }
-  return 0;
+    } } for (int i = 1; i <= n; i++) {
+  cout << f[a[i]] << ' ' << g[a[i]] << ' ' << n - f[(1 << B) - 1 - a[i]] << '\n'; }
 }
-// https://cses.fi/problemset/task/1654
