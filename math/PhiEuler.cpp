c681a951a22b9e813c79c14c3c8c7a401bc3c1c7
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-15 20:41:40 +0700

    Update PhiEuler.cpp

--- math/PhiEuler.cpp
+++ math/PhiEuler.cpp
@@ -13,3 +13,11 @@ int phi(int n) {
     }
     return ans;
 }
+
+vector<int> phi(n+1);
+for(int i = 1; i <= n; i++){
+    phi[i] += i;
+    for(int j = 2*i; j <= n; j += i){
+        phi[j] -= phi[i];
+    }
+}
