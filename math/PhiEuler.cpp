02043c4fec13904bf4262f922602ed4977ea1170
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-15 21:28:58 +0700

    Update PhiEuler.cpp

--- math/PhiEuler.cpp
+++ math/PhiEuler.cpp
@@ -2,18 +2,13 @@ int phi(int n) {
     int ans = n;
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
-            while (n % i == 0) {
-                n /= i;
-            }
+            while (n % i == 0) { n /= i; }
             ans -= ans / i;
         }
     }
-    if (n != 1) {
-        ans -= ans / n;
-    }
+    if (n != 1) { ans -= ans / n; }
     return ans;
 }
-
 vector<int> phi(n+1);
 for(int i = 1; i <= n; i++){
     phi[i] += i;
