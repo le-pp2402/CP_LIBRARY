d9aa3ba3e54c6712b6e019a5e972c3d17dfc6d43
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-15 21:28:03 +0700

    Update orderset.cpp

--- other/orderset.cpp
+++ other/orderset.cpp
@@ -1,3 +1,5 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
