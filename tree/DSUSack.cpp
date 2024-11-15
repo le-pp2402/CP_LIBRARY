175f753a8fee3ee7c94261c77c9d917b91bae687
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-15 10:43:54 +0700

    Update DSUSack.cpp

--- tree/DSUSack.cpp
+++ tree/DSUSack.cpp
@@ -1,7 +1,6 @@
 int sz[N];
 int cnt[N];
-void pre_dfs(int u, int p) { // Một hàm dfs chạy trước
-                             // để tính được độ lớn của từng cây con
+void pre_dfs(int u, int p) {
     sz[u] = 1;
     for (auto v : g[u]) if (v != p) {
         pre_dfs(v, u);
@@ -16,8 +15,8 @@ void dfs(int u, int p) {
     int bigChild = -1;
     for (auto v : g[u]) if (v != p) {
         if (bigChild == -1 || sz[v] > sz[bigChild]) bigChild = v;
-    } // Tìm cây con lớn nhất trong
-      // các con trực tiếp của đỉnh u
+    }
+    
     for (auto v : g[u]) if (v != p && v != bigChild) {
         dfs(v, u);
         update(v, u, -1);
@@ -25,6 +24,5 @@ void dfs(int u, int p) {
     if (bigChild != -1) dfs(bigChild, u);
     for (auto v : g[u]) if (v != p && v != bigChild) update(v, u, 1);
     cnt[color[u]]++;
-    // Trả lời các truy vấn tại đỉnh u, với cnt[c]
-    // là số lượng đỉnh có màu c trong cây con gốc u
+    // answer query
 }
