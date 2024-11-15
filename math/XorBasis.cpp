f8ef8be28711b706834d6259c520e8a13d4552c9
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-15 09:48:15 +0700

    Update XorBasis.cpp

--- math/XorBasis.cpp
+++ math/XorBasis.cpp
@@ -1,3 +1,18 @@
+// Cho ma tran M dem so ma tran x sao cho Mx = O (ma tran 0)
+// so ma tran x = 2^(nullity(M)) (tru 1 neu khong tinh ma tran 0)
+// rank(M) + nulltity(M) = so cot = n
+// nullity(M) = so cot - rank(M) = n - dim(span(a))
+// doan code o duoi tim duoc span -> dim = span.size() 
+// n - vec.size()
+
+void insertVector(int mask) {
+    for (int i = 0; i < (int)basis.size(); ++i) {
+    mask = min(mask, mask ^ basis[i]);
+    }
+    if (mask != 0) { basis.push_back(mask); }
+}
+
+
 struct Basis {
     int a[20] {};
     int t[20] {};
@@ -24,3 +39,4 @@ struct Basis {
         return x == 0;
     }
 };
+
