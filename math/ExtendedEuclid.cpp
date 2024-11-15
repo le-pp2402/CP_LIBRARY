8b21990dd86d498a42d0a1787e64cd3a97d82f84
le-pp2402 <107272462+le-pp2402@users.noreply.github.com>
2024-11-15 23:23:42 +0700

    Update ExtendedEuclid.cpp

--- math/ExtendedEuclid.cpp
+++ math/ExtendedEuclid.cpp
@@ -3,8 +3,5 @@
 // Phuong trinh tong quat ax + by = d chi co nghiem khi d chia het cho gcd(a, b).
 // a x + b y = gcd(a, b)
 template<typename T>
-T extgcd(T a, T b, T &x, T &y) {
-    T g = a; x = 1; y = 0;
-    if (b != 0) g = extgcd(b, a % b, y, x), y -= (a / b) * x;
-    return g;
-}
+T extgcd(T a, T b, T &x, T &y) { T g = a; x = 1; y = 0;
+    if (b != 0) g = extgcd(b, a % b, y, x), y -= (a / b) * x; return g; }
