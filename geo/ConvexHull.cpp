dce5b6aeebd3e7e378d55625da7e005606163bc2
hoanghao123code <21t1020347@husc.edu.vn>
2024-11-15 23:26:36 +0700

    update geo

--- geo/ConvexHull.cpp
+++ geo/ConvexHull.cpp
@@ -2,7 +2,6 @@
 bool ccw(const Point &A, const Point &B, const Point &C) {
     return 1LL * (B.x - A.x) * (C.y - A.y) - 1LL * (C.x - A.x) * (B.y - A.y) > 0;
 }
-
 // tra ve bao loi voi thu tu cac diem duoc liet ke theo chieu kim dong ho
 vector<Point> convexHull(vector<Point> p, int n) {
     sort(p.begin(), p.end(), [](const Point &A, const Point &B) {
@@ -12,15 +11,11 @@ vector<Point> convexHull(vector<Point> p, int n) {
     vector<Point> hull;
     hull.push_back(p[0]);
     for (int i = 1; i < n; ++i) {
-        while (hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), p[i])) {
-            hull.pop_back();
-        }
+        while (hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), p[i])) hull.pop_back();
         hull.push_back(p[i]);
     }
     for (int i = n - 2; i >= 0; --i) {
-        while (hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), p[i])) {
-            hull.pop_back();
-        }
+        while (hull.size() >= 2 && ccw(hull[hull.size() - 2], hull.back(), p[i])) hull.pop_back();
         hull.push_back(p[i]);
     }
     if (n > 1) hull.pop_back();
--- geo/PointLine.cpp
+++ geo/PointLine.cpp
@@ -2,21 +2,16 @@ struct Point {
   i64 x, y;
   Point() { x = y = 0.0; }
   Point(i64 x, i64 y) : x(x), y(y) {}
-
   Point operator+(const Point &a) const { return Point(x + a.x, y + a.y); }
   Point operator-(const Point &a) const { return Point(x - a.x, y - a.y); }
   Point operator*(i64 k) const { return Point(x * k, y * k); }
   Point operator/(i64 k) const { return Point(x / k, y / k); }
 };
-
 struct Line { // Ax + By = C
   i64 a, b, c;
   Line(i64 a = 0, i64 b = 0, i64 c = 0) : a(a), b(b), c(c) {}
   Line(Point A, Point B) {
-    a = B.y - A.y;
-    b = A.x - B.x;
-    c = a * A.x + b * A.y;
+    a = B.y - A.y; b = A.x - B.x; c = a * A.x + b * A.y;
   }
 };
-
 bool bl(Line line, Point p) { return (line.a * p.x + line.b * p.y == line.c); }
